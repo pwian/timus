028da7326751e0a06ff978a5e537ae128b2f18f9
Ивакин Александр Николаевич <Aleksandr_Ivakin@epam.com>
2020-03-30 17:55:22 +0300

    add email = pwian@mail.ru

---
+++ 1010/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _1010
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            long n = GetNumberInt();
+            long[] func = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                func[i] = GetNumberInt();
+            }
+            long left = 0, right = 1, koef = Math.Abs(func[left] - func[right]);
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (Math.Abs(func[i] - func[i + 1]) > koef)
+                {
+                    koef = Math.Abs(func[i] - func[i + 1]);
+                    left = i;
+                    right = i + 1;
+                }
+            }
+            Console.WriteLine("{0} {1}", left + 1, right + 1);
+
+        }
+    }
+}
