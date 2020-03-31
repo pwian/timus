d3fc4b944d9d7a5c2cc094e8f3015756d6ad090c
Ивакин Александр Николаевич <pwian@mail.ru>
2020-03-31 20:43:01 +0300

    1083

---
+++ 1083/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1083
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(line[0]);
+            int k = line[1].Length;
+            int result = n;
+            while (n > 0)
+            {
+                n -= k;
+                if (n > 0)
+                    result *= n;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
