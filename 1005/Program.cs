4b5beb19caf8ddd51d616417ff54ebcfa31a4461
Ивакин Александр Николаевич <pwian@mail.ru>
2020-04-02 17:56:41 +0300

    1005

---
+++ 1005/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1005
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int DivMin(int[] w, int fullSum, int currentSum, int k)
+        {
+            if (k >= w.Length - 1)
+                return Math.Abs(fullSum - 2 * currentSum);
+            if (2 * currentSum >= fullSum)
+                return Math.Abs(fullSum - 2 * currentSum);
+            return Math.Min(DivMin(w, fullSum, currentSum + w[k], k + 1), DivMin(w, fullSum, currentSum, k + 1));
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            int[] w = GetArrayInt();
+            Array.Sort(w);
+            int fullSum = 0; int sum2 = 0;
+            for (int i = 0; i < w.Length; i++)
+            {
+                fullSum += w[i];
+            }
+            int d = DivMin(w, fullSum, 0, 0);
+            Console.WriteLine(d);
+        }
+    }
+}
