64102e460a22eda82caf198905bd39625fd1ecf7
Ивакин Александр Николаевич <pwian@mail.ru>
2020-04-03 17:00:57 +0300

    1225

---
+++ 1225/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1225
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            long a2 = 1, a1 = 1;
+            long count = 0;
+            if (n == 1 || n == 2)
+                count = 2;
+            else
+            {
+                long buf;
+                for (int i = 2; i < n; i++)
+                {
+                    buf = a2 + a1;
+                    a1 = a2;
+                    a2 = buf;
+                }
+                count = 2 * a2;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
