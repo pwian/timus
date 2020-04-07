cb8cf3fb42133defdedac142ac6535ba522c6146
Aleksandr Ivakin <pwian@mail.ru>
2020-04-07 18:10:26 +0300

    1001

---
+++ 1001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> list = new List<long>();
+            string str = Console.In.ReadToEnd();
+            StringSplitOptions op = StringSplitOptions.RemoveEmptyEntries;
+            string[] stringArray = str.Split(new char[] { ' ', '\n', '\r', '\t' }, op);
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                list.Add(Convert.ToInt64(stringArray[i]));
+            }
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine("{0:f4}", Math.Sqrt(list[i]));
+            }
+        }
+    }
+}
