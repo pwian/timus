56c7e7719fb0b661bc5785a8c1dcf19cb7d1ed39
Aleksandr Ivakin <pwian@mail.ru>
2020-04-09 13:52:22 +0300

    1000

---
+++ 1000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(str[0]);
+            int b = Convert.ToInt32(str[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
