366b07cec1b706ed6f9b0b2dac4d10af0e5b3e51
Aleksandr Ivakin <pwian@mail.ru>
2020-04-15 13:14:11 +0300

    1068

---
+++ 1068/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+                Console.WriteLine((1 + n) * n / 2);
+            else if (n == 0)
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(n * (1 - n) / 2 + 1);
+        }
+    }
+}
