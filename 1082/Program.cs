96d56ca076295ab613c900c11362f1bc4a35ce56
Aleksandr Ivakin <pwian@mail.ru>
2020-04-21 18:02:38 +0300

    1082

---
+++ 1082/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1082
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+}
