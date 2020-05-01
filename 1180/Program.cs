0a6e1006dc95a5c358a9ce9218bb876efeb6f8b9
Aleksandr Ivakin <pwian@mail.ru>
2020-05-01 16:15:03 +0300

    1180

---
+++ 1180/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace _1180
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] number = Console.ReadLine().ToCharArray();
+            BigInteger n = new BigInteger();
+            for (int i = 0; i < number.Length; i++)
+            {
+                n = n * 10 + number[i] - '0';
+            }
+
+            if (n % 3 == 0)
+                Console.WriteLine("2");
+            else
+            {
+                Console.WriteLine("1");
+                Console.WriteLine(n % 3);
+
+            }
+        }
+    }
+}
