0a5b4a600e783e1974ecee45778d5a3c550ab9e0
Aleksandr Ivakin <pwian@mail.ru>
2020-05-05 11:19:16 +0300

    1224

---
+++ 1224/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1224
+{
+    class Program
+    {
+        static ulong[] GetArrayULong()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            ulong[] array = new ulong[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToUInt64(line[i]);
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            ulong[] array = GetArrayULong();
+            ulong n = array[0], m = array[1];
+            ulong countTurn;
+            if (n == 1)
+                countTurn = 0;
+            else if (m == 1)
+                countTurn = 1;
+            else
+            {
+                if (n <= m)
+                {
+                    countTurn = 2 * (n - 2) + 1;
+                    if ((countTurn + 1) / 2 != m)
+                        countTurn++;
+                }
+                else
+                {
+                    countTurn = 2 * (m - 2) + 2;
+                    if ((countTurn + 1) / 2 != n)
+                        countTurn++;
+                }
+            }
+
+            Console.WriteLine(countTurn);
+        }
+    }
+}
