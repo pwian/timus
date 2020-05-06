9b9875a979ac14cf19da2969137d83f975f148aa
Aleksandr Ivakin <pwian@mail.ru>
2020-05-06 17:40:56 +0300

    1243

---
+++ 1243/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace _1243
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger count;
+            BigInteger.TryParse(Console.ReadLine(), out count);
+
+            Console.WriteLine(count % 7);
+        }
+    }
+}
