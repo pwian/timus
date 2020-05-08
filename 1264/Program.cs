ff1ea37a87b0d45b57dbcffed38c0edce8386d83
Aleksandr Ivakin <pwian@mail.ru>
2020-05-08 18:03:26 +0300

    1264

---
+++ 1264/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1264
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(line[0]);
+            int m = Convert.ToInt32(line[1]);
+
+            Console.WriteLine(n * (m + 1));
+        }
+    }
+}
