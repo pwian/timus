5ba551c1fc6bbac4a2a9bdf40039b1f24ea38555
Aleksandr Ivakin <pwian@mail.ru>
2020-05-13 18:46:40 +0300

    1297

---
+++ 1297/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _1297
+{
+    class Program
+    {
+        static bool Polyndrom(string line)
+        {
+            for (int i = 0; i < line.Length / 2; i++)
+            {
+                if (line[i] != line[line.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string reverse = new String(text.ToCharArray().Reverse().ToArray());
+            string maxLine = "";
+            for (int i = 0; i < text.Length - maxLine.Length; i++)
+            {
+                string currentLine;
+                for (int j = i + maxLine.Length; j < text.Length; j++)
+                {
+                    currentLine = text.Substring(i, j - i + 1);
+                    if (reverse.Contains(currentLine))
+                    {
+                        if (Polyndrom(currentLine))
+                            maxLine = currentLine;
+                    }
+                    else
+                        break;
+
+                }
+            }
+            Console.WriteLine(maxLine);
+        }
+    }
+}
