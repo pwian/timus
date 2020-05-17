08fc32a8fa3719132828cfa60c6312c836e84966
Aleksandr Ivakin <pwian@mail.ru>
2020-05-17 11:12:48 +0300

    1313

---
+++ 1313/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _1313
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            int[,] matr = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] array = GetArrayInt();
+                for (int j = 0; j < n; j++)
+                {
+                    matr[i, j] = array[j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    Console.Write("{0} ", matr[j, i - j]);
+                }
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write("{0} ", matr[n - 1 + i - j, j]);
+                }
+            }
+
+        }
+    }
+}
