3436c57a6459c686eb7e232d9f0a1feea05aebb6
Aleksandr Ivakin <pwian@mail.ru>
2020-05-28 22:33:26 +0300

    1327

---
+++ 1327/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1327
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int day1 = GetNumberInt();
+            int day2 = GetNumberInt();
+            int count = (day2 + 1) / 2 - day1 / 2;
+            Console.WriteLine(count);
+        }
+    }
+}
