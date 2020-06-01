8592a5ca36ae2e5356364d45962ff1e793668f78
Aleksandr Ivakin <pwian@mail.ru>
2020-06-01 13:38:54 +0300

    1349

---
+++ 1349/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1349
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            if (n == 0 || n > 2)
+                Console.WriteLine("-1");
+            else if (n == 1)
+                Console.WriteLine("1 2 3");
+            else
+                Console.WriteLine("3 4 5");
+        }
+    }
+}
