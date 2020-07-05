48fa51d7a0192a3482362cb57c70a1ce76e2a3cd
Aleksandr Ivakin <pwian@mail.ru>
2020-07-05 14:09:01 +0300

    1409

---
+++ 1409/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+
+            Console.WriteLine("{0} {1}", Convert.ToInt32(line[1]) - 1, Convert.ToInt32(line[0]) - 1);
+        }
+    }
+}
