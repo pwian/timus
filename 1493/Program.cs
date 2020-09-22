bf630d6302d2b3ef54c8ecbc8af15a591a3f0526
Aleksandr Ivakin <pwian@mail.ru>
2020-09-22 20:30:54 +0300

    1493

---
+++ 1493/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1493
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static bool Lucky(int n)
+        {
+            int s1 = n % 1000;
+            int s2 = n / 1000;
+            s1 = s1 % 10 + s1 / 100 + (s1 / 10) % 10;
+            s2 = s2 % 10 + s2 / 100 + (s2 / 10) % 10;
+            return s1 == s2;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            if (Lucky(n - 1) || Lucky(n + 1))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
