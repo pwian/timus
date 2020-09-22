c40e08bc8524309e1a973da028ae6a085198d0ab
Aleksandr Ivakin <pwian@mail.ru>
2020-09-22 20:24:12 +0300

    1457

---
+++ 1457/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1457
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = GetNumberInt();
+            int[] array = GetArrayInt();
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine("{0:F6}", sum / n);
+        }
+    }
+}
