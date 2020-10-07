dc61b792decfd655f0a698524f572727d6bb0210
Aleksandr Ivakin <pwian@mail.ru>
2020-10-07 13:09:48 +0300

    1496

---
+++ 1496/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1496
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            Dictionary<string, int> list = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                string team = Console.ReadLine();
+                if (list.ContainsKey(team))
+                    list[team]++;
+                else
+                    list.Add(team, 1);
+            }
+            foreach (var item in list)
+            {
+                if (item.Value > 1)
+                    Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
