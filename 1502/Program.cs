ad4e9d89bf757ac02b4f079b8181f13689bb0dcf
Aleksandr Ivakin <pwian@mail.ru>
2020-10-09 13:24:02 +0300

    1502

---
+++ 1502/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1502
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            long a1 = (1 + n) * n / 2;
+            long countDiamond = a1;
+            for (int i = 1; i <= n; i++)
+            {
+                a1 = (i + n) * (n - i + 1) / 2;
+                countDiamond += a1 + i * (n - i + 1);
+            }
+            Console.WriteLine(countDiamond);
+        }
+    }
+}
