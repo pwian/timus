2bcd7c5ad42c1f4644cef7cae9b29dfb4216d628
Aleksandr Ivakin <pwian@mail.ru>
2020-10-09 13:28:30 +0300

    1563

---
+++ 1563/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1563
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            int countBayan = 0;
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string brand = Console.ReadLine();
+                if (list.Contains(brand))
+                    countBayan++;
+                else
+                    list.Add(brand);
+            }
+
+            Console.WriteLine(countBayan);
+        }
+    }
+}
