b570e85f916c08ec5f3a0ba0df3c7e01640ca8fc
Aleksandr Ivakin <pwian@mail.ru>
2020-10-09 13:32:22 +0300

    1567

---
+++ 1567/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1567
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int prize = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '.' || text[i] == ' ')
+                    prize++;
+                else if (text[i] == ',')
+                    prize += 2;
+                else if (text[i] == '!')
+                    prize += 3;
+                else
+                    prize += (text[i] - 'a') % 3 + 1;
+            }
+            Console.WriteLine(prize);
+
+        }
+    }
+}
