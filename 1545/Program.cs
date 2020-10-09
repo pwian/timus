736a5d2530215fac818d7c2106bcdd6cd02feb55
Aleksandr Ivakin <pwian@mail.ru>
2020-10-09 13:26:48 +0300

    1545

---
+++ 1545/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1545
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int n = GetNumberInt();
+            SortedList<string, int> list = new SortedList<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                string letters = Console.ReadLine();
+                if (!list.ContainsKey(letters))
+                    list.Add(letters, 1);
+            }
+            char c = Convert.ToChar(Console.ReadLine());
+            foreach (var item in list)
+            {
+                if (item.Key[0] == c)
+                    Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
