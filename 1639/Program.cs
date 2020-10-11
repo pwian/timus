131159d8376a8e281cf99d80ca5c3d0502ba2687
Aleksandr Ivakin <pwian@mail.ru>
2020-10-11 19:57:10 +0300

    1639

---
+++ 1639/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1639
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = GetArrayInt();
+            if ((a[0] * a[1] - 1) % 2 == 0)
+                Console.WriteLine("[second]=:]");
+            else
+                Console.WriteLine("[:=[first]");
+        }
+    }
+}
