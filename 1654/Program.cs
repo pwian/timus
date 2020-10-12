7423192a992a07dd5ead6852b78177c174bebbfd
Aleksandr Ivakin <pwian@mail.ru>
2020-10-12 19:57:08 +0300

    1654

---
+++ 1654/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _1654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder line = new StringBuilder(text.Length);
+            line.Append(text[0]);
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (line.Length > 0 && text[i] == line[line.Length - 1])
+                {
+                    line.Remove(line.Length - 1, 1);
+                }
+                else
+                    line.Append(text[i]);
+            }
+            Console.WriteLine(line);
+        }
+    }
+}
