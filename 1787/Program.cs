9e8e0ae6dcf4e88ad36460ae732df719c162ddde
Aleksandr Ivakin <pwian@mail.ru>
2020-10-31 15:30:51 +0300

    1787

---
+++ 1787/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1787
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int k = Convert.ToInt32(line[0]);
+            int n = Convert.ToInt32(line[1]);
+            int[] a = GetArrayInt();
+            int current = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                current += a[i] - k;
+                if (current < 0)
+                    current = 0;
+            }
+
+            Console.WriteLine(current);
+        }
+    }
+}
