d5a59cc224ce103f00f818d0905008a3496beac4
Aleksandr Ivakin <pwian@mail.ru>
2020-10-31 16:34:18 +0300

    1877

---
+++ 1877/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1877
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int code1 = Convert.ToInt32(Console.ReadLine());
+            int code2 = Convert.ToInt32(Console.ReadLine());
+            if (code1 % 2 == 0 || code2 % 2 == 1)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}
