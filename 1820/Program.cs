6b482c4072b5a0358291a070975c1fe54128f27f
Aleksandr Ivakin <pwian@mail.ru>
2020-10-31 15:33:20 +0300

    1820

---
+++ 1820/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1820
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(line[0]) * 2;
+            int k = Convert.ToInt32(line[1]);
+            int time = n / k;
+            if (n % k != 0)
+                time++;
+            if (n <= 2 * k)
+                time = 2;
+            Console.WriteLine(time);
+        }
+    }
+}
