478ae0aaffeeac3e798e744e323bb8f4283e9d26
Aleksandr Ivakin <pwian@mail.ru>
2020-11-01 20:09:49 +0300

    1991

---
+++ 1991/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _1991
+{
+    class Program
+    {
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static long GetNumberLong()
+        {
+            return Convert.ToInt64(Console.ReadLine());
+        }
+
+        static int[] GetArrayInt()
+        {
+            string[] @string = Console.ReadLine().Split(' ');
+            int[] array = new int[@string.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(@string[i]);
+            }
+            return array;
+        }
+
+        static long[] GetArrayLong()
+        {
+            string[] @string = Console.ReadLine().Split(' ');
+            long[] array = new long[@string.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt64(@string[i]);
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = GetArrayInt();
+            int n = array[0], k = array[1];
+            array = GetArrayInt();
+            int countUnused = 0;
+            int countSurvived = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (k > array[i])
+                    countSurvived += k - array[i];
+                else
+                    countUnused += array[i] - k;
+            }
+            Console.WriteLine("{0} {1}", countUnused, countSurvived);
+        }
+    }
+}
