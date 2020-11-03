f4c29d5ccaa108947b8ace742869029d765eb736
Aleksandr Ivakin <pwian@mail.ru>
2020-11-03 13:44:52 +0300

    2012

---
+++ 2012/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            if (t >= 7)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
