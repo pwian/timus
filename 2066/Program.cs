8dad2b2b274c3e3b190c8d99eeaf9504a8089148
Aleksandr Ivakin <pwian@mail.ru>
2020-11-07 14:06:08 +0300

    2066

---
+++ 2066/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2066
+{
+    class Program
+    {
+        static int[] GetArrayInt()
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int[] array = new int[line.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(line[i]);
+            }
+            return array;
+        }
+
+        static int GetNumberInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int a = GetNumberInt();
+            int b = GetNumberInt();
+            int c = GetNumberInt();
+            if (b == 0)
+                Console.WriteLine(a - b - c);
+            else if (b == 1)
+                Console.WriteLine(a - b - c);
+            else
+                Console.WriteLine(a - b * c);
+        }
+    }
+}
